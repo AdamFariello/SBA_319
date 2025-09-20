1420af125a4db071155e82c4cc73741d2eb0bd60
AdamFariello <ajf270@scarletmail.rutgers.edu>
2025-09-20 02:25:50 -0400

    Deleted other unneeded files

--- models/mediaSchema.mjs
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const mediaSchema = new mongoose.Schema({
-    id: {type: String, required: true},
-    pizzaID: {type: String, required: true},
-    userSubmitID: {type: String, required: true},
-    appetizingScore: {type: Number, required: true}
-});
-export default mongoose.model("Media", mediaSchema);
--- models/usersSchema.mjs
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const usersSchema = new mongoose.Schema({
-    userID: {type: String, required: true},
-    username: {type: String, required: true},
-    password: {type: String, required: true},
-})
-export default mongoose.model("Users", usersSchema);
