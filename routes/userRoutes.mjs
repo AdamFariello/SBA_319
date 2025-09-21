86ce11dd1196ed9555798d1e39bac9833e7e6d9e
AdamFariello <ajf270@scarletmail.rutgers.edu>
2025-09-21 17:22:25 -0400

    Added post method in userRoutes

--- routes/userRoutes.mjs
+++ routes/userRoutes.mjs
@@ -1,6 +1,7 @@
 import express from "express";
 import Users from "../models/usersSchema.mjs";
 import db from "../db/conn.mjs";
+import mongoose from "mongoose";
 
 const router = express.Router(); 
 let userColl = db.collection("user");
@@ -8,12 +9,36 @@ let userColl = db.collection("user");
 router.route("/")
       .get(async (req, res) => {
         let getUser;
-        if(req.query.user) {
+        if (req.query.user) {
             getUser = await userColl.find({"username":req.query.user}).toArray();
         } else {
             getUser = await userColl.find({}).toArray();
         }
         res.json(getUser);
+      })
+      .post(async (req, res) => {
+        if (req.body.username && req.body.password) {
+          //TODO: Figure out sometime how to use mongoose.model to create an entry
+          //let newUser = new mongoose.model("newUser", Users.schema.obj);
+          //newUser.username = req.body.username;
+          //newUser.password = req.body.password;
+          //newUser.save(null);
+          //await db.collection("pizza").insertMany(pizzaData);
+
+          //TODO: add a check to stop duplicate user entries
+          /*
+          {
+            "username":"username",
+            "password":"password"
+          }
+          */
+          let newUser = {
+            "username": req.body.username,
+            "password": req.body.password
+          }
+          userColl.insertOne(newUser);
+          res.json(newUser);
+        } else { res.json("ERROR: missing username or password")}
       })
 
 router.route("/:user")
