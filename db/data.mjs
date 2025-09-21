1282598c357466d939a878ecad81f41cec46fbd4
AdamFariello <ajf270@scarletmail.rutgers.edu>
2025-09-21 06:56:25 -0400

    Added 2 entries to the data file

---
+++ db/data.mjs
@@ -0,0 +1,49 @@
+
+const userData = [
+    {username:"cchalliner0",password:"lK3+Y(HeOP8"},
+    {username:"jellerman1",password:"mC3%0>(>d`~E!=k"},
+    {username:"dcargill2",password:"mH6{12,9YcH1"},
+    {username:"lmcgoon3",password:"bB7!uG/6L)m(J(J"},
+    {username:"eebbutt4",password:"wZ2)3@Ic_o*=.)"},
+    {username:"mpennetti5",password:"aC2=LbHL,uB{Nz"},
+    {username:"vdukelow6",password:"nZ6(|,}hEL*M}LE"},
+    {username:"ssmullin7",password:"hD9,7KW"},
+    {username:"vlabuschagne8",password:"sP5`o=)oLF"},
+    {username:"bscoggans9",password:"fI3=mSwWKD<a7Yh"},
+    {username:"kwisbeya",password:"oJ6>QueO~Lz?rmN"},
+    {username:"cnealandb",password:"hK4$D`?Zl1"},
+    {username:"nlafayc",password:"eA6\wHf|L"},
+    {username:"arichardind",password:"wN8(0Zt*O"},
+    {username:"misaqe",password:"oR8<E4RTm,.xcbf"},
+    {username:"mbonasf",password:"tP8\A`PY"},
+    {username:"gszapirog",password:"lO5*qf$>OvIq/jP"},
+    {username:"aaitonh",password:"cD6{$LJ#vHg@"},
+    {username:"feouzani",password:"xA5|2~>_KLRJe"},
+    {username:"abridywaterj",password:"gZ9/~'F4zKO/"},
+]
+
+const pizzaData=[
+    {pizzaName:"Margherita",pizzaLink:"http://dummyimage.com/209x100.png/cc0000/ffffff"},
+    {pizzaName:"Pepperoni",pizzaLink:"http://dummyimage.com/101x100.png/cc0000/ffffff"},
+    {pizzaName:"Four",pizzaLink:"http://dummyimage.com/218x100.png/cc0000/ffffff"},
+    {pizzaName:"Hawaiian",pizzaLink:"http://dummyimage.com/222x100.png/ff4444/ffffff"},
+    {pizzaName:"BBQ",pizzaLink:"http://dummyimage.com/115x100.png/dddddd/000000"},
+    {pizzaName:"Meat",pizzaLink:"http://dummyimage.com/148x100.png/5fa2dd/ffffff"},
+    {pizzaName:"Vegetarian",pizzaLink:"http://dummyimage.com/210x100.png/ff4444/ffffff"},
+    {pizzaName:"Buffalo",pizzaLink:"http://dummyimage.com/113x100.png/cc0000/ffffff"},
+    {pizzaName:"Supreme",pizzaLink:"http://dummyimage.com/138x100.png/ff4444/ffffff"},
+    {pizzaName:"White",pizzaLink:"http://dummyimage.com/138x100.png/cc0000/ffffff"},
+    {pizzaName:"Mushroom",pizzaLink:"http://dummyimage.com/136x100.png/cc0000/ffffff"},
+    {pizzaName:"Neapolitan",pizzaLink:"http://dummyimage.com/178x100.png/cc0000/ffffff"},
+    {pizzaName:"Chicago",pizzaLink:"http://dummyimage.com/161x100.png/5fa2dd/ffffff"},
+    {pizzaName:"New",pizzaLink:"http://dummyimage.com/234x100.png/ff4444/ffffff"},
+    {pizzaName:"Detroit",pizzaLink:"http://dummyimage.com/134x100.png/cc0000/ffffff"},
+    {pizzaName:"Tandoori",pizzaLink:"http://dummyimage.com/197x100.png/cc0000/ffffff"},
+    {pizzaName:"Seafood",pizzaLink:"http://dummyimage.com/106x100.png/cc0000/ffffff"},
+    {pizzaName:"Capricciosa",pizzaLink:"http://dummyimage.com/154x100.png/cc0000/ffffff"},
+    {pizzaName:"Greek",pizzaLink:"http://dummyimage.com/236x100.png/cc0000/ffffff"},
+    {pizzaName:"Breakfast",pizzaLink:"http://dummyimage.com/247x100.png/5fa2dd/ffffff"}
+]
+
+
+module.export({userData});
